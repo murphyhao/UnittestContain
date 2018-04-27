1f8f410fd779fe84517c4f3fcde3ce86e7fa9044
unknown <ecd1393@163.com>
2018-04-27 15:49:22 +0800

    create doc folder

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import unittest
 from calculator import Count
 
-class TestCount(unittest.TestCase):
+class TestAdd(unittest.TestCase):
 
     def setUp(self):
-        print("test start")
+        print("test add start")
 
     def tearDown(self):
-        print("test end")
+        print("test add end")
 
     def test_add(self):
         j = Count(2, 3)
@@ -17,12 +17,32 @@ class TestCount(unittest.TestCase):
         j = Count(41, 76)
         self.assertEqual(j.add(), 117)
 
+class TestSub(unittest.TestCase):
+
+    def setUp(self):
+        print("test sub start")
+
+    def tearDown(self):
+        print("test sub end")
+
+    def test_sub(self):
+        j = Count(2, 3)
+        self.assertEqual(j.sub(), -1)
+
+    def test_sub2(self):
+        j = Count(71, 46)
+        self.assertEqual(j.sub(), 25)
+
+
 if __name__ == '__main__':
     #unittest.main()
 
     # 构造测试集
     suite = unittest.TestSuite()
-    suite.addTest(TestCount("test_add2"))
+    suite.addTest(TestAdd("test_add"))
+    suite.addTest(TestAdd("test_add2"))
+    suite.addTest(TestSub("test_sub"))
+    suite.addTest(TestSub("test_sub2"))
 
     # 执行测试
     runner = unittest.TextTestRunner()
